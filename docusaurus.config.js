e814d83f4563196513fc920f2d27124f4e2e3515
Behnam Salehi <behnam.dev@outlook.com>
2023-11-07 23:47:41 +0330

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,6 +93,7 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ["csharp", "gherkin"],
       },
     }),
 };
